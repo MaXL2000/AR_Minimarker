ccaecf9899fb7b74d2cc1035f54338d66f688783
Maxim <MaXLyuT2000@gmail.com>
2021-01-04 13:21:14 +0300

    First commit
    Первый коммит

---
+++ minimarker/Assets/Script/LoadScenes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+	static private int queue = 1;
+    // Start is called before the first frame update
+    public void ChangeScenes()
+    {
+    	if (queue < 4){
+    		SceneManager.LoadScene(queue);
+    		queue = queue + 1;
+    	}
+    	else{
+    		queue = 0;
+    		SceneManager.LoadScene(queue);
+    		queue = queue + 1;
+    	}
+    }
+}
